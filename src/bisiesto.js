d641565fcabd5dc75c62115591a3f833196ce408
wilnermena <wi_an9@hotmail.com>
2023-08-19 23:39:03 -0400

    tdd-3

--- src/bisiesto.js
+++ src/bisiesto.js
@@ -12,6 +12,9 @@ function bisiesto(year){
   if(year%100===0){
     return false
   }
+  if(year%4===0){
+    return true
+  }
  
 }
   
--- src/bisiesto.test.js
+++ src/bisiesto.test.js
@@ -7,4 +7,7 @@ describe("AÑO BISIESTO", () => {
   it("Debería mostrar todos los años divisibles entre 100 pero no por 400 - BISIESTO", () => {
     expect(bisiesto(1000)).toEqual(false);
   });
+  it("Debería mostrar todos los años divisibles 4 pero no por 100 - BISIESTO", () => {
+    expect(bisiesto(2016)).toEqual(true);
+  });
 });
